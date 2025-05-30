83be7af2beba3d6437c361e22c506144532033ca
Peterson, Donald C [CSSM] <dcp@iastate.edu>
2025-05-30 15:02:01 -0500

    Latest fixes for installation

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [ -z "$VIRTUAL_ENV" ]; then
-  if [ -d "venv" ]; then
-    . venv/bin/activate
+  if [ -d ".venv" ]; then
+    . .venv/bin/activate
   else
-    read -p "Virtual environment not detected. Continue with installation? (y/N) " choice
+    read -p "Virtual environment not detected. Create one and continue with installation? (y/N) " choice
     case $choice in
       [yY])
+        python3 -m venv .venv
         break
         ;;
       *)
@@ -18,9 +19,7 @@ else
   echo "Virtual environment is active: $VIRTUAL_ENV"
 fi
 
-pip install -r requirements.txt
-mkdir files db
-python manage.py makemigrations accounts
-python manage.py makemigrations withings
-python manage.py migrate
-
+python3 -m pip install -r requirements.txt
+python3 manage.py makemigrations accounts
+python3 manage.py makemigrations withings
+python3 manage.py migrate
