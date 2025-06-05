99bfa71dab3c8d6b03190c7632408f5f41f7a101
Peterson, Donald C [CSSM] <dcp@iastate.edu>
2025-06-05 13:32:59 -0500

    Fixed missing db and files mkdir and fixed venv source

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
     case $choice in
       [yY])
         python3 -m venv .venv
-        break
+        . .venv/bin/activate
         ;;
       *)
         exit 0
@@ -19,6 +19,7 @@ else
   echo "Virtual environment is active: $VIRTUAL_ENV"
 fi
 
+mkdir db files
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations accounts
 python3 manage.py makemigrations withings
